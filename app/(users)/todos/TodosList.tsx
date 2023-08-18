164ce1d3a5ab0ccb208e503246988fef3761145b
Joshardals <joshuabamidele219@gmail.com>
2023-08-18 13:16:46 +0100

    finally done with it

--- app/(users)/todos/TodosList.tsx
+++ app/(users)/todos/TodosList.tsx
@@ -3,6 +3,11 @@ import Link from "next/link";
 import React from "react";
 
 const fetchToDos = async () => {
+  const randomTime = Math.floor(Math.random() * 4000) + 1000;
+  await new Promise((resolve) => {
+    setTimeout(resolve, randomTime);
+  });
+
   const res = await fetch(`https://jsonplaceholder.typicode.com/todos/`);
   const todos: Todo[] = await res.json();
   return todos;
